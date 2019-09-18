7d64a5761c28dd31fd499f9cebe3bec9872e7e44
D3SOX <d3sox@420blaze.it>
2019-09-18 19:54:31 +0200

    removed unnecessary files

--- CMakeFiles/Soundboard_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Soundboard_autogen"
-  "Soundboard_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Soundboard_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
