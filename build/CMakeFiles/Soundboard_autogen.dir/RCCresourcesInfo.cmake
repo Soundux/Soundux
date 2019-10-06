edcc6a582b64ca2dee8ad97b170303d6e533c896
D3SOX <nicolas.haid@gmail.com>
2019-10-06 13:09:20 +0200

    added docs and updated readme

---
+++ build/CMakeFiles/Soundboard_autogen.dir/RCCresourcesInfo.cmake
@@ -0,0 +1,18 @@
+# Configurations
+set(ARCC_MULTI_CONFIG "FALSE")
+set(ARCC_VERBOSITY "")
+# Settings file
+set(ARCC_SETTINGS_FILE "/home/nico/Soundboard/build/CMakeFiles/Soundboard_autogen.dir/RCCresourcesSettings.txt")
+# Directories
+set(ARCC_BUILD_DIR "/home/nico/Soundboard/build/Soundboard_autogen")
+set(ARCC_INCLUDE_DIR "/home/nico/Soundboard/build/Soundboard_autogen/include")
+# Rcc executable
+set(ARCC_RCC_EXECUTABLE "/usr/bin/rcc")
+set(ARCC_RCC_LIST_OPTIONS "--list")
+# Rcc job
+set(ARCC_LOCK_FILE "/home/nico/Soundboard/build/CMakeFiles/Soundboard_autogen.dir/RCCresources.lock")
+set(ARCC_SOURCE "/home/nico/Soundboard/resources.qrc")
+set(ARCC_OUTPUT_CHECKSUM "EWIEGA46WW")
+set(ARCC_OUTPUT_NAME "qrc_resources.cpp")
+set(ARCC_OPTIONS "-name;resources")
+set(ARCC_INPUTS "/home/nico/Soundboard/icon.jpg")
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+ # Soundboard
+ ### Soundboard is a universal soundboard in Qt for linux using pulseaudio modules
+ 
+ # Features
+ 
+ - Simple UI
+ - Saves configuration automatically
+ - Uses PulseAudio modules so it should work on almost any distribution
+ - Output to any recording stream
+ - more to come...
+
+# Screenshots
+to be added...
