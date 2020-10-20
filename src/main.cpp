3236dd1b9411d2c18f59a14d497e3debbf323f72
D3SOX <d3sox@protonmail.com>
2020-10-20 15:25:12 +0200

    feat: initial qt quick setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
 
-int main()
+int main(int argc, char **argv)
 {
-    return 0;
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    QQuickStyle::setStyle("Material");
+
+    engine.load("qrc:/main.qml");
+    return app.exec();
 }
---
+++ src/main.qml
@@ -0,0 +1,11 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+import QtQuick.Controls.Material 2.15
+ApplicationWindow
+{
+    visible: true
+    width: 640
+    height: 480
+    title: "Soundux"
+    Material.theme: Material.Dark
+}
