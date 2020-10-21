6022295dd9a0d8ae614d253045642fae36e37a2c
Curve <fynnbwdt@gmail.com>
2020-10-21 16:11:03 +0200

    Implemented Hotkeys

--- src/hotkeys/linux.h
+++ src/hotkeys/linux.h
@@ -4,3 +4,99 @@
     This will probably fix #12 and #15
 
 */
+#pragma once
+#include <atomic>
+#include <thread>
+#include <iostream>
+// #include <X11/XKBlib.h>
+// #include <X11/extensions/XInput2.h>
+
+namespace Soundux
+{
+    namespace Hooks
+    {
+        namespace internal
+        {
+            inline std::thread keyListener;
+            inline std::atomic<bool> killThread = false;
+
+            class Display;
+            inline void hook()
+            {
+                Display *display = XOpenDisplay(":0");
+                if (display == NULL)
+                {
+                    std::cerr << "Failed to open X11 Display" << std::endl;
+                    return;
+                }
+
+                int xiOpCode, queryEvent, queryError;
+                if (!XQueryExtension(display, "XInputExtension", &xiOpCode, &queryEvent, &queryError))
+                {
+                    std::cerr << "XInput extension is not aviable" << std::endl;
+                    return;
+                }
+
+                {
+                    int major, minor = 0;
+                    int queryResult = XIQueryVersion(display, &major, &minor);
+                    if (queryResult == BadRequest)
+                    {
+                        std::cerr << "XI 2.0 support is required - Current Version: " << major << "." << minor
+                                  << std::endl;
+                        return;
+                    }
+                    else if (queryResult != Success)
+                    {
+                        std::cerr << "Unknown error" << std::endl;
+                        return;
+                    }
+                }
+
+                Window root = DefaultRootWindow(display);
+                XIEventMask mask;
+                mask.deviceid = XIAllMasterDevices;
+                mask.mask_len = XIMaskLen(XI_LASTEVENT);
+                mask.mask = calloc(m.mask_len, sizeof(char));
+
+                XISetMask(mask.mask, XI_RawKeyPress);
+                XISelectEvents(display, root, &mask, 1);
+                XSync(display, false);
+                free(mask.mask);
+
+                while (!killThread.load())
+                {
+                    XEvent event;
+                    XGenericEventCookie *cookie = (XGenericEventCookie *)&event.xcookie;
+                    XNextEvent(display, &event);
+
+                    if (XGetEventData(display, cookie) && cookie->type == GenericEvent && cookie->extension == xiOpCode)
+                    {
+                        XIRawEvent *ev = cookie->data;
+                        auto key = ev->detail;
+
+                        KeySym s = XkbKeycodeToKeysym(display, key, 0, 0);
+                        if (NoSymbol == s)
+                            continue;
+                        char *str = XKeysymToString(s);
+                        if (NULL == str)
+                            continue;
+
+                        std::cout << str << std::endl;
+                    }
+                }
+            }
+        } // namespace internal
+
+        inline void setup()
+        {
+            internal::keyListener = std::thread(internal::hook);
+        }
+        inline void stop()
+        {
+            internal::killThread.store(true);
+            internal::keyListener.join();
+        }
+
+    } // namespace Hooks
+} // namespace Soundux
--- src/hotkeys/windows.h
+++ src/hotkeys/windows.h
@@ -1,5 +1,92 @@
 /*
 
     Will use SetWindowsHookEx with WH_KEYBOARD_LL
+    To find the sound that uses this hotkey, we will have a map of all sounds that have hotkeys bound to them, the key
+    of that map will be the last key that has to be pressed for the sound to play, then we check all the previous keys
+    with GetAsyncKeyState ( if that works, if it doesn't well save the keystates from the KeyBoard hook in a map )
+    The Hotkey-Map should be able to be used without a mutex - since the only time we access it outside of the keyboard
+    thread is the time we load all the sounds and at that time the keyboard hook doesn't even run.
 
 */
+#pragma once
+#include <Windows.h>
+#include <thread>
+#include <atomic>
+
+namespace Soundux
+{
+    namespace Hooks
+    {
+        namespace internal
+        {
+            inline HHOOK oKeyBoardProc;
+            inline std::thread keyListener;
+            inline std::atomic<bool> killThread = false;
+
+            // ! May be useful later (for displaying the keys in the ui [?])
+            // inline std::string VirtualKeyCodeToString(UCHAR virtualKey)
+            // {
+            //     UINT scanCode = MapVirtualKey(virtualKey, MAPVK_VK_TO_VSC);
+
+            //     CHAR name[128];
+            //     int result = 0;
+            //     switch (virtualKey)
+            //     {
+            //     case VK_LEFT:
+            //     case VK_UP:
+            //     case VK_RIGHT:
+            //     case VK_DOWN:
+            //     case VK_RCONTROL:
+            //     case VK_RMENU:
+            //     case VK_LWIN:
+            //     case VK_RWIN:
+            //     case VK_APPS:
+            //     case VK_PRIOR:
+            //     case VK_NEXT:
+            //     case VK_END:
+            //     case VK_HOME:
+            //     case VK_INSERT:
+            //     case VK_DELETE:
+            //     case VK_DIVIDE:
+            //     case VK_NUMLOCK:
+            //         scanCode |= KF_EXTENDED;
+            //     default:
+            //         result = GetKeyNameTextA(scanCode << 16, name, 128);
+            //     }
+            //
+            //     return name;
+            // }
+
+            inline LRESULT CALLBACK LLkeyBoardProc(int nCode, WPARAM wParam, LPARAM lParam)
+            {
+                if (nCode == HC_ACTION)
+                {
+                    if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
+                    {
+                        PKBDLLHOOKSTRUCT info = reinterpret_cast<PKBDLLHOOKSTRUCT>(lParam);
+                        // TODO: Process keyEvent here.
+                    }
+                }
+                return CallNextHookEx(oKeyBoardProc, nCode, wParam, lParam);
+            }
+        } // namespace internal
+        inline void setup()
+        {
+            internal::keyListener = std::thread([&] {
+                internal::oKeyBoardProc = SetWindowsHookEx(WH_KEYBOARD_LL, internal::LLkeyBoardProc, NULL, NULL);
+                MSG message;
+                while (GetMessage(&message, NULL, 0, 0) && !internal::killThread.load())
+                {
+                    TranslateMessage(&message);
+                    DispatchMessage(&message);
+                }
+                UnhookWindowsHookEx(internal::oKeyBoardProc);
+            });
+        }
+        inline void stop()
+        {
+            internal::killThread.store(true);
+            internal::keyListener.join();
+        }
+    } // namespace Hooks
+} // namespace Soundux
