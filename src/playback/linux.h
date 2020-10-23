4aa2e890e4def814e8876cf7d3a0015b3e7a0702
Curve <fynnbwdt@gmail.com>
2020-10-23 16:08:12 +0200

    feat: add linux playback boilerplate

--- src/playback/linux.h
+++ src/playback/linux.h
@@ -8,4 +8,42 @@
 
 */
 #ifdef __linux___
+#include <exception>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <memory>
+#include <vector>
+#include <array>
+
+namespace Soundux
+{
+    namespace Playback
+    {
+        namespace internal
+        {
+            inline std::string getOutput(const std::string &command)
+            {
+                std::array<char, 128> buffer;
+                std::string result;
+                std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
+                if (!pipe)
+                {
+                    throw std::exception("popen failed");
+                    return "";
+                }
+                while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+                {
+                    result += buffer.data();
+                }
+                return result;
+            }
+
+            inline void createSink();
+            inline void deleteSink();
+            inline void getDefaultCaptureDevice();
+            inline std::vector<std::string> getSources();
+        } // namespace internal
+    }     // namespace Playback
+} // namespace Soundux
 #endif
