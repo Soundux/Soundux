1a4fe4bdd11e1f4025162c7e5bab88d3584a38ac
Curve <fynnbwdt@gmail.com>
2020-12-17 21:36:41 +0100

    fix: removed brace that was placed by clangd...

--- src/playback/linux.h
+++ src/playback/linux.h
@@ -28,7 +28,7 @@ namespace Soundux
                 std::string result;
 
 #ifdef FLATPAK
-                std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(("flatpak-spawn --host" + command).c_str()), "r"), pclose);
+                std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(("flatpak-spawn --host" + command).c_str(), "r"), pclose);
 #else
                 std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
 #endif
