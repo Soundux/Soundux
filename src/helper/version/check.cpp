59cf1be72ddc6ba70ea9d2013e2f5412483c5d10
Curve <fynnbwdt@gmail.com>
2021-03-24 18:55:16 +0100

    feat: also check for beta versions in update check (closes #105)

--- src/helper/version/check.cpp
+++ src/helper/version/check.cpp
@@ -16,30 +16,12 @@ std::optional<Soundux::Objects::VersionStatus> VersionCheck::getStatus()
 
         if (!parsed.is_discarded())
         {
-            auto _latestTag = parsed[0]["name"];
-            if (!_latestTag.is_null())
+            auto latestTag = parsed[0]["name"];
+            if (!latestTag.is_null())
             {
-                static const std::regex versionRegex(R"(^([0-9. ]+).*$)");
-                std::smatch match;
-
-                auto latestTag = _latestTag.get<std::string>();
-                if (std::regex_search(latestTag, match, versionRegex))
-                {
-                    if (match[1].matched)
-                    {
-                        return Soundux::Objects::VersionStatus{SOUNDUX_VERSION, match[1], match[1] != SOUNDUX_VERSION};
-                    }
-                    Fancy::fancy.logTime().warning() << "Failed to fetch latest version" << std::endl;
-                }
-                else
-                {
-                    Fancy::fancy.logTime().warning() << "Failed to parse latest version" << std::endl;
-                }
+                return Soundux::Objects::VersionStatus{SOUNDUX_VERSION, latestTag, latestTag != SOUNDUX_VERSION};
             }
-            else
-            {
-                Fancy::fancy.logTime().warning() << "Failed to find latest tag" << std::endl;
-            }
+            Fancy::fancy.logTime().warning() << "Failed to find latest tag" << std::endl;
         }
         else
         {
