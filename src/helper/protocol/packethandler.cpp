8404311ec43bfe6c05d909a70ba08202923634db
Persson <sim16.prib@gmail.com>
2021-04-29 16:59:44 +0200

    style: use lower case for function handlePacket

--- src/helper/protocol/packethandler.cpp
+++ src/helper/protocol/packethandler.cpp
@@ -5,7 +5,7 @@ namespace Soundux
 {
     namespace Objects
     {
-        void PacketHandler::HandlePacket(const Network::Protocol::PlaySoundPacket &packet)
+        void PacketHandler::handlePacket(const Network::Protocol::PlaySoundPacket &packet)
         {
             auto pSound = Globals::gGui->playSound(packet.getSoundId());
             if (pSound)
@@ -13,7 +13,7 @@ namespace Soundux
                 Globals::gGui->onSoundPlayed(*pSound);
             }
         }
-        void PacketHandler::HandlePacket(const Network::Protocol::StopSoundsPacket &)
+        void PacketHandler::handlePacket(const Network::Protocol::StopSoundsPacket &)
         {
             if (!Globals::gAudio.getPlayingSounds().empty())
             {
