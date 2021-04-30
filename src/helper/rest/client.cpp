7eefad960b7d2077e53e10e0139e4d14096cb387
Persson <sim16.prib@gmail.com>
2021-04-30 14:16:09 +0200

    refactor: remove use of stringstream

--- src/helper/rest/client.cpp
+++ src/helper/rest/client.cpp
@@ -2,7 +2,6 @@
 
 #include <core/global/globals.hpp>
 #include <httplib.h>
-#include <sstream>
 
 namespace Soundux
 {
@@ -14,9 +13,7 @@ namespace Soundux
         void SounduxClient::playSound(std::uint32_t soundID)
         {
             Client cl("localhost", Globals::gConfig.settings.serverPort);
-            std::ostringstream stringStream;
-            stringStream << "/playsound/" << soundID;
-            cl.Get(stringStream.str().c_str());
+            cl.Get(std::string("/playsound/" + std::to_string(soundID)).c_str());
         }
         void SounduxClient::stopSounds()
         {
