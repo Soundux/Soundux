e78f17fa6ede36023ac30a461b6d7a283e45f2df
D3SOX <nico@d3sox.me>
2022-05-27 18:46:21 +0200

    Revert "feat(pulseaudio): mute microphone instead of sink"
    This reverts commit e4bfce692fa8d325e9ee5c9e8b040cc01e319abe.

--- src/helper/audio/linux/pulseaudio/pulseaudio.cpp
+++ src/helper/audio/linux/pulseaudio/pulseaudio.cpp
@@ -144,8 +144,7 @@ namespace Soundux::Objects
 
         fetchLoopBackSinkId();
 
-        if (!nullSink || !loopBack || !loopBackSink || !passthrough || !passthroughSink || !passthroughLoopBack ||
-            !defaultSourceId)
+        if (!nullSink || !loopBack || !loopBackSink || !passthrough || !passthroughSink || !passthroughLoopBack)
         {
             unloadLeftOvers();
             return false;
@@ -193,17 +192,6 @@ namespace Soundux::Objects
                 {
                     reinterpret_cast<PulseAudio *>(userData)->defaultSource = info->default_source_name;
                     reinterpret_cast<PulseAudio *>(userData)->serverName = info->server_name;
-                }
-            },
-            this));
-        await(PulseApi::context_get_sink_input_info_list(
-            context,
-            []([[maybe_unused]] pa_context *ctx, const pa_sink_input_info *info, [[maybe_unused]] int eol,
-               void *userData) {
-                auto *thiz = reinterpret_cast<PulseAudio *>(userData);
-                if (info && info->name == thiz->defaultSource)
-                {
-                    thiz->defaultSourceId = info->index;
                 }
             },
             this));
@@ -642,7 +630,7 @@ namespace Soundux::Objects
         bool success = false;
 
         await(PulseApi::context_set_sink_input_mute(
-            context, *defaultSourceId, state,
+            context, *loopBackSink, state,
             +[]([[maybe_unused]] pa_context *ctx, int success, void *userData) {
                 *reinterpret_cast<bool *>(userData) = success;
             },
