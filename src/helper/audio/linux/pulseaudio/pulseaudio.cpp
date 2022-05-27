611ebcc216daabbdb5747dfed9dee71fa90a52bb
D3SOX <nico@d3sox.me>
2022-05-27 18:44:25 +0200

    Revert "fix(pulseaudio): mute the default audio source directly"
    This reverts commit 71eb3a90dfba09813b0bfe11dab2fd63e58ca322.

--- src/helper/audio/linux/pulseaudio/pulseaudio.cpp
+++ src/helper/audio/linux/pulseaudio/pulseaudio.cpp
@@ -144,7 +144,8 @@ namespace Soundux::Objects
 
         fetchLoopBackSinkId();
 
-        if (!nullSink || !loopBack || !loopBackSink || !passthrough || !passthroughSink || !passthroughLoopBack)
+        if (!nullSink || !loopBack || !loopBackSink || !passthrough || !passthroughSink || !passthroughLoopBack ||
+            !defaultSourceId)
         {
             unloadLeftOvers();
             return false;
@@ -192,6 +193,17 @@ namespace Soundux::Objects
                 {
                     reinterpret_cast<PulseAudio *>(userData)->defaultSource = info->default_source_name;
                     reinterpret_cast<PulseAudio *>(userData)->serverName = info->server_name;
+                }
+            },
+            this));
+        await(PulseApi::context_get_sink_input_info_list(
+            context,
+            []([[maybe_unused]] pa_context *ctx, const pa_sink_input_info *info, [[maybe_unused]] int eol,
+               void *userData) {
+                auto *thiz = reinterpret_cast<PulseAudio *>(userData);
+                if (info && info->name == thiz->defaultSource)
+                {
+                    thiz->defaultSourceId = info->index;
                 }
             },
             this));
@@ -632,8 +644,8 @@ namespace Soundux::Objects
     {
         bool success = false;
 
-        await(PulseApi::context_set_source_mute_by_name(
-            context, defaultSource.c_str(), state,
+        await(PulseApi::context_set_sink_input_mute(
+            context, *defaultSourceId, state,
             +[]([[maybe_unused]] pa_context *ctx, int success, void *userData) {
                 *reinterpret_cast<bool *>(userData) = success;
             },
@@ -641,7 +653,7 @@ namespace Soundux::Objects
 
         if (!success)
         {
-            Fancy::fancy.logTime().failure() << "Failed to mute default source" << std::endl;
+            Fancy::fancy.logTime().failure() << "Failed to mute loopback sink" << std::endl;
         }
 
         return success;
